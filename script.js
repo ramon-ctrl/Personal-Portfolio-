c42238764947edacc600d8f27d12bad30e44456a
ramon-ctrl <ramonmagana34@icloud.com>
2023-05-15 13:27:35 -0700

    Slide-up/Script Code

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script code
     var typed = new Typed(".typing", {
-        strings: ["Web Designer", "Marketer", "Graphic Designer"],
+        strings: ["Web Developer", "Marketer", "Graphic Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web Designer", "Marketer", "Graphic Designer"],
+        strings: ["Web Developer", "Marketer", "Graphic Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
