89fe76b93afd6da8dbd23344a086b568461a6869
ramon-ctrl <ramonmagana34@icloud.com>
2023-05-15 10:23:28 -0700

    Editing Images/CSS

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script code
     var typed = new Typed(".typing", {
-        strings: ["Web Designer", "YouTuber", "Graphic Designer"],
+        strings: ["Web Designer", "Marketer", "Graphic Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web Designer", "YouTuber", "Graphic Designer"],
+        strings: ["Web Designer", "Marketer", "Graphic Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
