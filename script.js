6245385e164ff9b4ecd662a6d3bde7a1df6c9ca8
ramon-ctrl <ramonmagana34@icloud.com>
2023-05-15 15:33:37 -0700

    Project Descriptions/Edits on About Page

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script code
     var typed = new Typed(".typing", {
-        strings: ["Web Developer", "Marketer", "Graphic Designer"],
+        strings: ["Web Developer", "Marketer", "Graphic Designer","Writer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web Developer", "Marketer", "Graphic Designer"],
+        strings: ["Web Developer", "Marketer", "Graphic Designer","Writer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
